a1f6b9cfb1fefa57b162275420f63205696f1b6c
Rafael Ribas <rafa.ribas02@gmail.com>
2021-03-08 00:07:25 -0300

    initializing the snake on the screen

--- Snake_old.py
+++ Snake_old.py
@@ -7,6 +7,8 @@ screen = pygame.display.set_mode((600,600))
 pygame.display.set_caption('Snake')
 
 snake = [(200, 200), (210, 200), (220, 200)]
+snake_skin = pygame.Surface((10, 10))
+snake_skin.fill((0, 200, 0))
 
 while (True):
 
@@ -14,5 +16,10 @@ while (True):
 
         if event.type == QUIT:
             pygame.quit()
+
+    screen.fill((0, 0, 0))
+
+    for position in snake:
+        screen.blit(snake_skin, position)
 
     pygame.display.update()
